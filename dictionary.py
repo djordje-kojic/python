11a238f2d9bd68941dc2e63f1fdcf1d521c65c4b
djordje-kojic <kojicdjordje@gmail.com>
2023-05-12 17:45:14 +0200

    Final version

--- dictionary.py
+++ dictionary.py
@@ -4,6 +4,7 @@ import json
 # Print menu function
 #----------------------------------------
 def print_menu():
+    print('\n')
     for key in menu_options.keys():
         print (key, '--', menu_options[key] ) 
         
@@ -12,7 +13,7 @@ def print_menu():
 # ----------------------------------------
 def createNewWord ():
     word = input("Enter a word: ")
-    meaning = input("Ënter meaning: \n")
+    meaning = input("Ënter meaning:")
     
     
     if word not in dictionary:
@@ -21,13 +22,25 @@ def createNewWord ():
         dictionary[word].append(meaning)
 
 
-# Save and Quit function
-#-----------------------------------------
-def saveAndQuit ():
-    with open("dictionary.json", "w") as outfile:
-        outfile.write(json.dumps(dictionary, indent=4))
-        outfile.close()
-        
+#Change function
+#----------------------------------------
+def changeWord():
+    wordToChange = input("Enter the word you want to change: ")
+    if wordToChange in dictionary:
+        newMeaning = input("Enter a new value: ")
+        dictionary[wordToChange] = [newMeaning]
+    else:
+        print("\nNo such word!\n")
+
+
+#Find function
+#----------------------------------------
+def find ():
+    findWord = input("Enter the word: ")
+    for x in dictionary.keys():
+        if findWord in x:
+            print('\n', x , ': ' ,  dictionary[x])
+
 
 #Delete function
 #----------------------------------------
@@ -37,24 +50,14 @@ def delWord ():
         dictionary.pop(word_toDel)
         print("\n Successfully deleted \n")
 
-    
-#Change function
-#----------------------------------------
-def changeWord():
-    wordToChange = input("Enter the word you want to change: ")
-    newMeaning = input("Enter a new value:")
-    if wordToChange in dictionary:
-        dictionary[wordToChange] = [newMeaning]
 
-#Find function
-#----------------------------------------
-def find ():
-    findWord = input("Enter the word: ")
-    """ if findWord in dictionary:
-        print(findWord , ': ' ,  dictionary[findWord]) """
-    for x in dictionary.keys():
-        if findWord in x:
-            print(x , ': ' ,  dictionary[x])
+# Save and Quit function
+#-----------------------------------------
+def saveAndQuit ():
+    with open("dictionary.json", "w") as outfile:
+        outfile.write(json.dumps(dictionary, indent=4))
+        outfile.close()
+        
 
             
 
@@ -73,14 +76,20 @@ menu_options = {
 
 
 while(True):
-    print_menu()
-    option = int(input('\nEnter your choice: '))
 
+    print_menu()
+
+    try:
+        option = int(input('\nEnter your choice: '))
+    except ValueError:
+        print("\n!!! Wrong entry, try again (need to be number) !!! \n")
+        continue
+    
     match option:
         case 1:
             createNewWord()        
         case 2:
-            print(dictionary)
+            print(dictionary, '\n')
         case 3:
             changeWord()
         case 4:
